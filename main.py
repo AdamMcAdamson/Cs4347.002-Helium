0cfe0e0413b7c56dfd58d028aff62a509703c189
Adam Brunn <AdamMcAdamson@gmail.com>
2022-10-08 18:02:36 -0500

    remove node_modules

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, current_app
+from flask_restful import Resource, Api
+import sqlite3 as sql
+
+app = Flask(__name__, static_folder='public',
+                      static_url_path='/public' )
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+class Quote(Resource):
+    def get(self):
+        args = request.args
+        with sql.connect('HeliumDB.db') as conn:
+            return conn.execute('SELECT * FROM quote WHERE id = ?', args['id']).fetchall()
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Quote, '/quote', endpoint='quote')
+
+if __name__ == '__main__':
+    app.run(debug=True)
