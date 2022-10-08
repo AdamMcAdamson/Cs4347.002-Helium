a24efa39a7038a36422bf1e1c586c3ec45e5305a
Adam Brunn <AdamMcAdamson@gmail.com>
2022-10-08 17:56:03 -0500

    add website and cleanup

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import sqlite3 as sql
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+class Quote(Resource):
+    def get(self):
+        args = request.args
+        with sql.connect('HeliumDB.db') as conn:
+            return conn.execute('SELECT * FROM quote WHERE id = ?', args['id']).fetchall()
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Quote, '/quote', endpoint='quote')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ public/scripts/api_calls.js
@@ -0,0 +1,31 @@
+function getQuotes(){
+    var res_loc = document.getElementById('res');
+    var list = document.getElementById('res-list');
+
+    fetch("quotes").then(response => {
+        return response.json();
+    }).then(res => {
+        var data = res.data
+        
+        // clear list
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+        
+        // populate list
+        for (var i = 0; i < data.length; i++){
+            list.innerHTML += `<li class='list-group-item'>` + data[i].quote + " - " + data[i].author + `</li>`;
+        }
+        
+        // insert response
+        res_loc.textContent = JSON.stringify(data, null, 2);
+        
+        console.log(data);
+    }).catch(err => {
+        // Do something for an error here
+    });
+}
+
+var button = document.getElementById("res-btn");
+
+button.addEventListener("click", getQuotes);
