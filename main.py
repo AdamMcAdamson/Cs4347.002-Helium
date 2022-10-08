27ed2d4d5abecec35d9b0ffe24beefd21b322716
Adam Brunn <AdamMcAdamson@gmail.com>
2022-10-08 18:14:48 -0500

    Fix static files

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, current_app
 from flask_restful import Resource, Api
 import sqlite3 as sql
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/')
 api = Api(app)
 
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'Hello': "World"}
 
 class Quote(Resource):
     def get(self):
