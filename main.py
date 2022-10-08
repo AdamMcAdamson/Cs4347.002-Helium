f5699ae16b61a412859d55e92e6b840da573e140
PureAsbestos <aquajonah@gmail.com>
2022-10-08 18:47:13 -0500

    Automatically serve index.html

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, current_app
+from flask import Flask, request
 from flask_restful import Resource, Api
 import sqlite3 as sql
 
-app = Flask(__name__, static_url_path='/')
+app = Flask(__name__, static_url_path='')
 api = Api(app)
 
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'Hello': "World"}
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 class Quote(Resource):
     def get(self):
@@ -17,7 +17,6 @@ class Quote(Resource):
             return conn.execute('SELECT * FROM quote WHERE id = ?', args['id']).fetchall()
 
 
-api.add_resource(HelloWorld, '/')
 api.add_resource(Quote, '/quote', endpoint='quote')
 
 if __name__ == '__main__':
