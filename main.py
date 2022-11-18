0bb66dcf0e3773fbeeb5307543c3bc5cab978661
Adam Brunn <AdamMcAdamson@gmail.com>
2022-11-18 14:03:48 -0600

    Repair parser and update db

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 import sqlite3 as sql
+import traceback, sys
 
 app = Flask(__name__, static_url_path='/')
 api = Api(app)
 
-
-@app.route('/')
-def index():
+def index(): 
     return app.send_static_file('index.html')
+
+app.add_url_rule('/', 'index', index)
+
+with sql.connect('HeliumDB.db') as conn:
+    file = open('./resources/schema.sql', mode = 'r', encoding='utf-8')
+    c = conn.cursor()
+    c.executescript(file.read())
 
 class Quote(Resource):
     def get(self):
@@ -16,10 +22,36 @@ class Quote(Resource):
         with sql.connect('HeliumDB.db') as conn:
             conn.row_factory = sql.Row
             c = conn.cursor()
+            #return jsonify({"message": 200, "success":True, "data": [dict(x) for x in (c.execute('SELECT * FROM quote').fetchall())]})
             return [dict(x) for x in (c.execute('SELECT * FROM quote').fetchall())]
+
+def create_borrower():
+    args = request.args
+
+    ssn = args.get("ssn")
+    bname = args.get("bname")
+    address = args.get("address")
+    phone = args.get("phone", "NULL")
+
+    stuff = "SSN: " + ssn + "\tBNAME: " + bname + "\tADDRESS: " + address + "\tPHONE: " + phone
+    return stuff,203
+    with sql.connect('HeliumDB.db') as conn:
+        conn.row_factory = sql.Row
+        c = conn.cursor()
+        # @TODO: Check for existing SSN and return useful error if it already exists
+        # command = "INSERT INTO BORROWER (Ssn, Bname, Address, Phone) VALUES VALUES (?, ?, ?, ?);",
+        # c.execute(
+        #     command,
+        #     (ssn, bname, address, phone)
+        # )
+        # + borrower[0] + "', '" + borrower[1] + "', '" + borrower[2]+ "', '" + borrower[3] + "')"        
+        # return [dict(x) for x in (c.execute('SELECT * FROM quote').fetchall())]
+        return app.make_response(200)
 
 
 api.add_resource(Quote, '/quote', endpoint='quote')
+
+app.add_url_rule('/borrower/create', 'create_borrower', create_borrower, methods=["POST"])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- scripts/db_parser.py
+++ scripts/db_parser.py
@@ -44,7 +44,9 @@ with open('./resources/books.csv', mode = 'r', encoding='utf-8') as file:
         for j in line["Author"].split(','):
             if j in authors:
                 index = authors.index(j)
-                book_authors.append([index, line["ISBN10"]])
+                book_author = [index, line["ISBN10"]]
+                if book_author not in book_authors:
+                    book_authors.append([index, line["ISBN10"]])
             else:
                 authors.append(j)
                 # Author ID is 1 indexed in database
@@ -66,61 +68,65 @@ with open('./resources/borrowers.csv', mode = 'r', encoding='utf-8') as file:
 # Write Schema File
 with open('./resources/schema.sql', mode = 'w+', encoding='utf-8') as file:
 
-    header = '''
-    CREATE TABLE BOOK (
-        Isbn CHAR(10) NOT NULL,
-        Title VARCHAR NOT NULL,
-        Cover_url VARCHAR NOT NULL,
-        PRIMARY KEY (Isbn) 
-    );
+    header = '''DROP TABLE IF EXISTS BOOK;
 
-    CREATE TABLE AUTHORS (
-        Author_id INT NOT NULL AUTOINCREMENT,
-        Name VARCHAR NOT NULL,
-        PRIMARY KEY (Author_id) 
-    );
+CREATE TABLE BOOK (
+    Isbn CHAR(10) PRIMARY KEY,
+    Title VARCHAR NOT NULL,
+    Cover_url VARCHAR NOT NULL
+);
 
-    CREATE TABLE BOOK_AUTHORS (
-        Author_id INT NOT NULL,
-        Isbn CHAR(10) NOT NULL,
-        PRIMARY KEY (Author_id, Isbn),
-        FOREIGN KEY (Author_id) REFERENCES AUTHORS(Author_id),
-        FOREIGN KEY (Isbn) REFERENCES BOOK(Isbn)
-    );
+DROP TABLE IF EXISTS AUTHORS;
 
-    CREATE TABLE BORROWER (
-        Card_id INT NOT NULL AUTOINCREMENT,
-        Ssn CHAR(11) NOT NULL UNIQUE,
-        Bname VARCHAR NOT NULL,
-        Address VARCHAR NOT NULL,
-        Phone CHAR(14),
-        PRIMARY KEY (Card_id)
-    );
-    '''
+CREATE TABLE AUTHORS (
+    Author_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name VARCHAR NOT NULL
+);
+
+DROP TABLE IF EXISTS BOOK_AUTHORS;
+
+CREATE TABLE BOOK_AUTHORS (
+    Author_id INTEGER NOT NULL,
+    Isbn CHAR(10) NOT NULL,
+    PRIMARY KEY (Author_id, Isbn),
+    FOREIGN KEY (Author_id) REFERENCES AUTHORS (Author_id),
+    FOREIGN KEY (Isbn) REFERENCES BOOK (Isbn)
+);
+
+DROP TABLE IF EXISTS BORROWER;
+
+CREATE TABLE BORROWER (
+    Card_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Ssn CHAR(11) NOT NULL UNIQUE,
+    Bname VARCHAR NOT NULL,
+    Address VARCHAR NOT NULL,
+    Phone CHAR(14)
+);
+'''
 
     file.write(header)
 
     # Populate BOOK Table
-    file.write("\n\tINSERT INTO BOOK (Isbn, Title)")
-    for book in books:
-        file.write("\n\tVALUES ('" + book[0] + "', '" + book[1] + "', '" + book[2] + "')")
-    file.write(";\n")
+    file.write("\nINSERT INTO BOOK (Isbn, Title, Cover_url)\n\tVALUES")
+    for book in books[:-1]:
+        file.write("\n\t\t('" + book[0] + "', '" + book[1] + "', '" + book[2] + "'),")
+    file.write("\n\t\t('" + books[-1][0] + "', '" + books[-1][1] + "', '" + books[-1][2] + "');\n")
 
     # Populate AUTHORS Table
-    file.write("\n\tINSERT INTO AUTHORS (Name)")
-    for author in authors:
-        file.write("\n\tVALUES ('" + author + "')")
-    file.write(";\n")
+    file.write("\nINSERT INTO AUTHORS (Name)\n\tVALUES")
+    for author in authors[:-1]:
+        file.write("\n\t\t('" + author + "'),")
+    file.write("\n\t\t('" + authors[-1] + "');\n")
 
     # Populate BOOK_AUTHORS Table
-    file.write("\n\tINSERT INTO BOOK_AUTHORS (Author_id, Isbn)")
-    for book_author in book_authors:
-        file.write("\n\tVALUES (" + str(book_author[0]) + ", '" + book_author[1] + "')")
-    file.write(";\n")
+    file.write("\nINSERT INTO BOOK_AUTHORS (Author_id, Isbn)\n\tVALUES")
+    for book_author in book_authors[:-1]:
+        file.write("\n\t\t(" + str(book_author[0]) + ", '" + book_author[1] + "'),")
+    file.write("\n\t\t(" + str(book_authors[-1][0]) + ", '" + book_authors[-1][1] + "');\n")
 
     # Populate BORROWER Table
-    file.write("\n\tINSERT INTO BORROWER (Ssn, Bname, Address, Phone)")
-    for borrower in borrowers:
-        file.write("\n\tVALUES ('"+ borrower[0] + "', '" + borrower[1] + "', '" + borrower[2]+ "', '" + borrower[3] + "')")
-    file.write(";\n")
+    file.write("\nINSERT INTO BORROWER (Ssn, Bname, Address, Phone)\n\tVALUES")
+    for borrower in borrowers[:-1]:
+        file.write("\n\t\t('"+ borrower[0] + "', '" + borrower[1] + "', '" + borrower[2]+ "', '" + borrower[3] + "'),")
+    file.write("\n\t\t('"+ borrowers[-1][0] + "', '" + borrowers[-1][1] + "', '" + borrowers[-1][2]+ "', '" + borrowers[-1][3] + "');\n")
         
