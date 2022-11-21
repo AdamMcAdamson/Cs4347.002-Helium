fa791abcc0b3aec2e43136e73a43f1b37f81e2c8
Adam Brunn <AdamMcAdamson@gmail.com>
2022-11-21 11:11:34 -0600

    Borrower management (#25)
    * Borrower Create creation and non-duplicate ssn
    
    * Fixed borrower insert bugs
    
    * added new borrower form
    
    * create borrower form
    
    * create new borrower form
    
    * Update requirements.txt
    
    * Cleanup comments
    
    * Use route decorator
    
    * Repair db file
    
    * Borrower Create creation and non-duplicate ssn
    
    * Fixed borrower insert bugs
    
    * added new borrower form
    
    * create borrower form
    
    * create new borrower form
    
    * Update requirements.txt
    
    * Cleanup comments
    
    * Use route decorator
    
    * Repair db file
    
    * Rebase with develop
    
    Co-authored-by: lanaejenkins1 <29955358+lanaejenkins1@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,48 +1,65 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template, flash, redirect
 from flask_restful import Resource, Api
+from forms import NewBorrowerForm
+from views import views_blueprint
 import sqlite3 as sql
 
 from consts import DB_FILE
 from search import Search
 
-app = Flask(__name__, static_url_path='/')
+app = Flask(__name__)
 api = Api(app)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
+# Register the blueprints 
+app.register_blueprint(views_blueprint, url_prefix='/views')
+
+@app.route('/', methods=['GET'])
 def index(): 
     return app.send_static_file('index.html')
 
-app.add_url_rule('/', 'index', index)
+with sql.connect('HeliumDB.db') as conn:
+    file = open('./resources/schema.sql', mode = 'r', encoding='utf-8')
+    c = conn.cursor()
+    c.executescript(file.read())
 
+class Quote(Resource):
+    def get(self):
+        args = request.args
+        with sql.connect('HeliumDB.db') as conn:
+            conn.row_factory = sql.Row
+            c = conn.cursor()
+            #return jsonify({"message": 200, "success":True, "data": [dict(x) for x in (c.execute('SELECT * FROM quote').fetchall())]})
+            return [dict(x) for x in (c.execute('SELECT * FROM quote').fetchall())]
 
-#TODO: refactor as a Borrower Resource with a .put method 
+@app.route('/borrower/create', methods=['GET', 'POST'])
 def create_borrower():
-    args = request.args
+    form = NewBorrowerForm()
+    #args = request.args
+    if form.validate_on_submit():
+        ssn = request.form["ssn"]
+        bname = request.form["name"]
+        address = request.form["address"]
+        phone = request.form["phone"]
 
-    ssn = args.get("ssn")
-    bname = args.get("bname")
-    address = args.get("address")
-    phone = args.get("phone", "NULL")
+        with sql.connect('HeliumDB.db') as conn:
+            conn.row_factory = sql.Row
+            c = conn.cursor()
+            # Check for existing SSN and return useful error if it already exists
+            if c.execute('SELECT * FROM BORROWER WHERE Ssn = ?', (ssn,)).fetchone():
+                flash(f'Only one borrower card per person. Ssn must be unique.', 'danger')
+                return render_template('new_borrower.html', form=form, title='Create New Borrower')
+            # Create borrower
+            command = "INSERT INTO BORROWER (Ssn, Bname, Address, Phone) VALUES (?, ?, ?, ?);"
+            c.execute(
+                command,
+                (ssn, bname, address, phone,)
+            )
+            flash(f'Account created for {form.name.data}!', 'success')
+            # @TODO add redirection page
+    return render_template('new_borrower.html', form=form, title='Create New Borrower')
 
-    stuff = "SSN: " + ssn + "\tBNAME: " + bname + "\tADDRESS: " + address + "\tPHONE: " + phone
-    return stuff,203
-    with sql.connect(DB_FILE) as conn:
-        conn.row_factory = sql.Row
-        c = conn.cursor()
-        # @TODO: Check for existing SSN and return useful error if it already exists
-        # command = "INSERT INTO BORROWER (Ssn, Bname, Address, Phone) VALUES VALUES (?, ?, ?, ?);",
-        # c.execute(
-        #     command,
-        #     (ssn, bname, address, phone)
-        # )
-        # + borrower[0] + "', '" + borrower[1] + "', '" + borrower[2]+ "', '" + borrower[3] + "')"        
-        # return [dict(x) for x in (c.execute('SELECT * FROM quote').fetchall())]
-        return app.make_response(200)
-
-app.add_url_rule('/borrower/create', 'create_borrower', create_borrower, methods=["POST"])
-
-
-api.add_resource(Search, '/book/search', endpoint='search')
-
+api.add_resource(Quote, '/quote', endpoint='quote')
 
 if __name__ == '__main__':
 
