d2cd73d9389a27fc19b203604bd320d0fa3ff9c5
Adam Brunn <AdamMcAdamson@gmail.com>
2022-11-27 13:53:50 -0600

    Reorganize frontend and setup initial form validation

---
+++ public/scripts/form_validation.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", () => {
+  'use strict'
+
+  // @TODO: Include validation code for each form which requires it
+  // and remove global default validation code below:
+  /*
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+  
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+  .forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })  
+  */
+
+  var borrowerManagementForm = document.querySelector('#borrower-management-form')
+  borrowerManagementForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    if (!borrowerManagementForm.checkValidity()) {
+      event.stopPropagation()
+    }
+    // @TODO: Call borrower/create endpoint and handle duplicate SSN error
+    borrowerManagementForm.classList.add('was-validated')
+  }, false)
+
+
+  var bookSearchForm = document.querySelector('#book-search-form')
+  bookSearchForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    // @TODO: Fetch Search endpoint and populate table
+  }, false)
+})
